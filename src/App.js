b90774616b0c014183ff88f1d0ce4d37153d7760
akashsharma081 <akashsharma@gmail.com>
2021-11-25 17:25:08 +0530

    Dummy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Route ,Routes} from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Services from './Components/Services';
+import Product from './Components/Product';
+import Items from './Components/Items';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className="App">
+  {/* <Home/> */}
+  <BrowserRouter> 
+     <Header />   
+      <Routes> 
+          <Route path='/Home' element={<Home/>} />
+          <Route path='/About'  element={ <About />}/>
+          <Route path='/Contact'  element={ <Contact />}/>
+          <Route path='/Services'  element={ <Services />}/>
+          <Route path='/Product'  element={ <Product />}/>
+          <Route path='/Items'  element={ <Items />}/> 
+      </Routes> 
+          <Footer/>
+  </BrowserRouter>
+</div>
   );
 }
 
